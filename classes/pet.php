a958ff7b9f73f84cb30df3dd6d03998f5d1f64e8
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-02-14 12:24:45 -0500

    adding dog class and pets class

---
+++ classes/pet.php
@@ -0,0 +1,32 @@
+<?php
+class Pet{
+    private $_name;
+    private $_color;
+
+    //Parameterized constructor
+    function __construct($name="unknown", $color="?")
+    {
+        $this->_name = $name;
+        $this->_color = $color;
+    }
+
+    function eat()
+    {
+        echo $this->_name . " is eating<br>";
+    }
+
+    function talk()
+    {
+        echo $this->_name . " is talking<br>";
+    }
+
+    function getName()
+    {
+        return $this->_name;
+    }
+
+    function setName($name)
+    {
+        $this->_name = $name;
+    }
+}
